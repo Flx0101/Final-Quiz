ef465cbd954cc5c7f32764cb4b7b89c003d6ed02
Flx0101 <codevelop0101@gmail.com>
2018-05-14 01:37:48 +0530

    Basic_Quiz_App
    Google India Scholarship Project.

---
+++ app/src/main/java/com/example/knight/quiz_udacity/EditTextQuestion.java
@@ -0,0 +1,13 @@
+package com.example.knight.quiz_udacity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EditTextQuestion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_text_question);
+    }
+}
---
+++ app/src/main/java/com/example/knight/quiz_udacity/ViewScore.java
@@ -0,0 +1,40 @@
+package com.example.knight.quiz_udacity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.graphics.Typeface;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class ViewScore extends AppCompatActivity {
+
+    @Override
+    public void onBackPressed() {
+        Intent backToMain = new Intent(ViewScore.this, MainActivity.class);
+        startActivity(backToMain);
+        finish();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_score);
+
+        Bundle getScore = getIntent().getExtras();
+        assert getScore != null;
+        int score = getScore.getInt("score");
+
+        Typeface greetingFont = Typeface.createFromAsset(getAssets(), "fonts/century.ttf");
+
+        TextView greetingTextView = findViewById(R.id.greeting_text_view);
+        greetingTextView.setTypeface(greetingFont);
+        TextView scoreTextView = findViewById(R.id.score_text_view);
+        scoreTextView.setTypeface(greetingFont);
+
+        scoreTextView.setText(score + "/10");
+
+
+    }
+}
